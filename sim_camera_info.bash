320107d3838432fd0d20feab61ec9216d91f0e11
André Russo <andrerusso02@gmail.com>
2023-12-01 17:32:01 +0100

    add script to simulate missing camera_info_topic

---
+++ sim_camera_info.bash
@@ -0,0 +1,19 @@
+rostopic pub /forwardCamera/camera_info sensor_msgs/CameraInfo "header:
+  seq: 0
+  stamp: {secs: 0, nsecs: 0}
+  frame_id: "camera_forward_optical_frame"
+height: 771
+width: 1028
+distortion_model: "Mei"
+D: [0.8803200125694275]
+K: [1124.66943359375, 0.0, 505.781982421875, 0.0, 1124.6165771484375, 387.8110046386719, 0.0, 0.0, 1.0]
+R: [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]
+P: [1124.66943359375, 0.0, 505.781982421875, 0.0, 0.0, 1124.6165771484375, 387.8110046386719, 0.0, 0.0, 0.0, 1.0, 0.0]
+binning_x: 1
+binning_y: 1
+roi: 
+  x_offset: 0
+  y_offset: 0
+  height: 771
+  width: 1028
+  do_rectify: False"
